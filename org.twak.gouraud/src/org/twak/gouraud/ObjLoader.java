6ae94c6d290899f037e9dd769a2a645996ce47f6
tom kelly <twak@users.noreply.github.com>
2018-01-28 22:38:09 +0000

    Update ObjLoader.java

--- org.twak.gouraud/src/org/twak/gouraud/ObjLoader.java
+++ org.twak.gouraud/src/org/twak/gouraud/ObjLoader.java
@@ -259,23 +259,6 @@ public class ObjLoader {
         specularColorUniform = glGetUniformLocationARB(program, "uSpecularColor");
     }
     
-    /*
-     *     float ambientStrength = 0.5;
-    float diffuseStrength = 0.5;
-    float specularStrength = 0.5;
-    float shininess = 4.0;
-    vec3 ambientColor = ambientStrength * uAmbientColor;
-    vec3 normal = normalize(vNormal);
-    vec3 lightDirection = normalize(uLightPosition - vPosition);
-    vec3 diffuseColor = diffuseStrength * max(0.0, dot(normal, lightDirection)) * uDiffuseColor;
-    vec3 viewDirection = normalize(uViewPosition - vPosition);
-    vec3 reflectDirection = reflect(-lightDirection, normal);
-    vec3 specularColor = specularStrength
-            * pow(max(dot(viewDirection, reflectDirection), 0.0), shininess) * uSpecularColor;
-            *     gl_FragColor = vec4(1,0,0,1);//ambientColor + diffuseColor + specularColor, 1.0);
-
-     */
-
     void update() {
         projectionMatrix.setPerspective((float) Math.toRadians(fov), (float) width / height, 0.01f,
                 100.0f);
